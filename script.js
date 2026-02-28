2ff8ed062f47ca53a69a8c7d54f51a713ab2696d
Izaque Santos Rocha <izaquesr901@gmail.com>
2026-02-28 12:56:00 -0300

    arrumando deletar tarefas

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
 const tarefas = []
 
-function criarTarefaNaTela(texto) {
+function criarTarefaNaTela(tarefa) {
     let li = document.createElement("li")
-    li.innerHTML = '<input type="checkbox" class="checkbox">' + texto + '<span onclick="deletarTarefa(this)">✕</span>'
+
+    li.innerHTML = `
+        <input type="checkbox" class="checkbox" ${tarefa.concluida ? "checked" : ""}>
+        ${tarefa.texto}
+        <span>✕</span>
+    `
+
+    li.querySelector("span").addEventListener("click", () => {
+        deletarTarefa(tarefa.id)
+    })
+
     document.querySelector("ul").appendChild(li)
 }
 
@@ -18,19 +28,32 @@ function adicionarTarefa() {
     if (input === "") {
         alert("Nenhuma tarefa foi adicionada")
         return
-    } else {
-        tarefas.push(input)
-        localStorage.setItem("tarefas", JSON.stringify(tarefas))
-
-        criarTarefaNaTela(input)
-
     }
+
+    const novaTarefa = {
+        id: Date.now(),
+        texto: input,
+        concluido: false
+    }
+
+    tarefas.push(novaTarefa)
+    localStorage.setItem("tarefas", JSON.stringify(tarefas))
+
+    criarTarefaNaTela(novaTarefa)
 
     document.querySelector('input').value = ""
 }
 
-function deletarTarefa(li) {
-    li.parentElement.remove()
-    localStorage.removeItem("tarefas")
+function deletarTarefa(id) {
+    const index = tarefas.findIndex(tarefa => tarefa.id === id)
+
+    if (index !== -1) {
+        tarefas.splice(index, 1)
+    }
+
+    localStorage.setItem("tarefas", JSON.stringify(tarefas))
+
+    document.querySelector("ul").innerHTML = ""
+    tarefas.forEach(tarefa => criarTarefaNaTela(tarefa))
 
 }
